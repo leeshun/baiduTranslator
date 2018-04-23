3232bd749c533255dd5a38043bdf748318d7fe52
leeshun <leeshun1230@gmail.com>
2018-04-24 00:30:46 +0800

    add varaible constants

---
+++ src/baidutranslator/common/HttpClient.java
@@ -0,0 +1,11 @@
+package baidutranslator.common;
+
+import java.net.HttpURLConnection;
+
+/**
+ * Created by leeshun on 2018/4/24.
+ */
+public class HttpClient {
+    private HttpURLConnection connection;
+
+}
---
+++ src/baidutranslator/constants/BaiduConstants.java
@@ -0,0 +1,32 @@
+package baidutranslator.constants;
+
+/**
+ * Created by leeshun on 2018/4/24.
+ */
+public class BaiduConstants {
+    public static final String BAIDU_URL = "http://api.fanyi.baidu.com/api/trans/vip/translate";
+
+    public static final String SECURE_BAIDU_URL = "https://api.fanyi.baidu.com/api/trans/vip/translate";
+
+    public static final String SEGMENT_QUERY = "q";
+
+    public static final String SEGMENT_FROM = "from";
+
+    public static final String SEGMENT_TO = "to";
+
+    public static final String SEGMENT_APPID = "appid";
+
+    public static final String SEGMENT_SALT = "salt";
+
+    public static final String SEGMENT_SIGN = "sign";
+
+    public static final String RESPONSE_FROM = "from";
+
+    public static final String RESPONSE_TO = "to";
+
+    public static final String RESPONSE_TRANSCATION_RESULT = "trans_result";
+
+    public static final String RESPONSE_RESULT_SOURCE = "src";
+
+    public static final String RESPONSE_RESULT_DESTIONATION = "dst";
+}
